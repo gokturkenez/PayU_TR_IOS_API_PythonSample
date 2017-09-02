6ea2a2f2a776af7e3ba816619dc7114d1affd2a3
gokturkenez <hi@gokturkenez.com.tr>
2017-09-02 18:23:39 +0300

    Release

---
+++ README.md
@@ -0,0 +1,15 @@
+# PayU Turkey IOS API Python 3.x Sample Code
+
+PayU Turkey IOS API Python 3 Sample Code v1.0
+    
+    * This is an **unofficial** sample code.
+    * This version is a release version, there is can be some fix needs, just let me know if you face 
+    with something wrong.
+    * This sample code developed on Python 3.4.2
+    * Below libraries used when developing
+        hmac
+        hashlib
+        urllib.parse > urlencode
+        urllib.request > Request, urlopen
+    * We are not owner any used libraries, all rights reserved to the owners.
+    * Datetime can be changed due to your system.
---
+++ ios.py
@@ -0,0 +1,45 @@
+'''
+Project: PayU Turkey Card Info v2 Python3 Sample Code
+Author: Göktürk Enez
+'''
+# Importing required libraries.
+import hmac
+import hashlib
+from urllib.parse import urlencode
+from urllib.request import Request, urlopen
+
+#Endpoint
+url = 'https://secure.payu.com.tr/order/ios.php'
+
+#Secret Key
+secret = 'SECRET_KEY'
+
+array = {
+'MERCHANT': 'OPU_TEST',
+'REFNOEXT': '57786',
+}
+# Initializing the hashstring @param
+hashstring = ''
+
+# Sorting Array @params
+for k, v in sorted(array.items()):
+
+# Adding the length of each field value at the beginning of field value
+    hashstring += str(len(v)) + str(v)
+print(hashstring)
+
+# Calculating ORDER_HASH
+signature = hmac.new(secret.encode('utf-8'), hashstring.encode('utf-8'), hashlib.md5).hexdigest()
+
+# Adding ORDER_HASH @param to request array
+array['HASH'] = signature
+
+print(signature)
+
+# Sending Request to Endpoint
+request = Request(url, urlencode(array).encode())
+response = urlopen(request).read().decode()
+
+# Printing result/response
+print(response)
+
